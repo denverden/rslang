02ae4037bb44021ed124a8ba35d1d10307723006
lena zamnius <lena.zamnius@gmail.com>
2020-06-19 10:38:51 +0300

    feat: add dictionary page template

---
+++ src/components/dictionary/dictionary.js
@@ -0,0 +1,7 @@
+import './dictionary.scss';
+
+const { body } = document;
+const text = document.createElement('p');
+
+text.innerHTML = '<p class="wordlist__word">dictionary</p>';
+body.appendChild(text);
