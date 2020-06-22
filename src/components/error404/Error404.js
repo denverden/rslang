d830082e5a3fb087af172baa8392c45067aa474a
Denis Novikov <denverden@mail.ru>
2020-06-22 22:54:12 +0300

    feat: added example, html-loader and rule eslint

--- src/components/error404/Error404.js
+++ src/components/error404/Error404.js
@@ -1,10 +1,26 @@
 import Component from '../Component';
 
-class Error404 extends Component { }
+class Error404 extends Component {
+  // eslint-disable-next-line class-methods-use-this
+  beforeRender() {
+    // тут можно делать что-то до отрисовки компонента
+    console.log('before render');
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  afterRender() {
+    // тут можно делать что-то после отрисовки компонента
+    console.log('after render');
+    document.querySelector('.js-click').addEventListener('click', (event) => {
+      event.preventDefault();
+      window.location.hash = '#';
+    });
+  }
+}
 
 const error404 = new Error404({
   selector: 'main',
-  template: '<h1>404 Page</h1>',
+  template: '<h1>404 Page</h1><a class="btn btn-primary js-click">На главную</a>',
 });
 
 export default error404;
