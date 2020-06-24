67a6079ef4cb8008d976f1d159d716669471c9cb
Denis Novikov <denverden@mail.ru>
2020-06-24 23:06:42 +0300

    feat: added singin page

--- src/components/routing/routesData.js
+++ src/components/routing/routesData.js
@@ -1,10 +1,12 @@
 import homePage from '../home-page/HomePage';
 import aboutPage from '../about-page/AboutPage';
+import signinPage from '../signin-page/SignInPage';
 import error404 from '../error404/Error404';
 
 const routes = [
   { path: '', components: [homePage] },
   { path: 'about', components: [aboutPage] },
+  { path: 'sign-in', components: [signinPage] },
   { path: '***', components: [error404] },
 ];
 
---
+++ src/components/signin-page/SignInPage.js
@@ -0,0 +1,10 @@
+import Component from '../Component';
+
+class SignInPage extends Component { }
+
+const signinPage = new SignInPage({
+  selector: 'main',
+  template: '<h1>SignIn Page</h1>',
+});
+
+export default signinPage;
