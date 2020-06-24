ed04e872d800f1733631442483c8dfd4b5836492
Denis Novikov <denverden@mail.ru>
2020-06-24 22:43:51 +0300

    feat: added games page

--- src/components/routing/routesData.js
+++ src/components/routing/routesData.js
@@ -1,10 +1,12 @@
 import homePage from '../home-page/HomePage';
 import aboutPage from '../about-page/AboutPage';
+import gamesPage from '../games-page/GamesPage';
 import error404 from '../error404/Error404';
 
 const routes = [
   { path: '', components: [homePage] },
   { path: 'about', components: [aboutPage] },
+  { path: 'games', components: [gamesPage] },
   { path: '***', components: [error404] },
 ];
 
