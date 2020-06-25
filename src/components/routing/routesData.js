956624b970eb075a5f6862ed3c953c6e31a1696a
Denis Novikov <denverden@mail.ru>
2020-06-26 01:07:00 +0300

    feat: implemented signup

--- src/components/routing/routesData.js
+++ src/components/routing/routesData.js
@@ -1,12 +1,14 @@
 import homePage from '../home-page/HomePage';
 import aboutPage from '../about-page/AboutPage';
 import signinPage from '../signin-page/SignInPage';
+import signupPage from '../signup-page/SignUpPage';
 import error404 from '../error404/Error404';
 
 const routes = [
   { path: '', components: [homePage] },
   { path: 'about', components: [aboutPage] },
   { path: 'sign-in', components: [signinPage] },
+  { path: 'sign-up', components: [signupPage] },
   { path: '***', components: [error404] },
 ];
 
