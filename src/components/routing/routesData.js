d6aa74663bc567d29976fff839375ee52d5d0169
Anscfl <anscarfull@mail.ru>
2020-07-03 16:42:20 +0400

    fix: add new responsive

--- src/components/routing/routesData.js
+++ src/components/routing/routesData.js
@@ -3,15 +3,6 @@ import homePage from '../home-page/HomePage';
 import aboutPage from '../about-page/AboutPage';
 import gamesPage from '../games-page/GamesPage';
 import error404 from '../error404/Error404';
-<<<<<<< HEAD
-import promoPage from '../promo-page/PromoPage';
-
-const routes = [
-  { path: '', components: [homePage] },
-  { path: 'about', components: [aboutPage] },
-  { path: '***', components: [error404] },
-  { path: 'promo', components: [promoPage] },
-=======
 import settingsPage from '../settings-page/SettingsPage';
 import signinPage from '../signin-page/SignInPage';
 import signupPage from '../signup-page/SignUpPage';
@@ -24,7 +15,6 @@ const routes = [
   { path: 'sign-up', components: [header, signupPage] },
   { path: 'settings', components: [header, settingsPage] },
   { path: '***', components: [header, error404] },
->>>>>>> 86ec9252b8afaffc6d1356e2af2d09784dfb0f53
 ];
 
 export default routes;
