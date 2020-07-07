0e5ce3da51636478f1754e49150989aca6d8579d
DenverDen <denverden@mail.ru>
2020-07-07 19:51:16 +0300

    Revert "Feature messages"

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import app from './components/App';
-
-import '../node_modules/bootstrap/dist/js/bootstrap';
-import AppStore from './components/AppStore';
-
-AppStore.loadSettings().then(() => app.start());
