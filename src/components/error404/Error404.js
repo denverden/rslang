edea6c8d9dc257f02c9475f1f2c24f73fa156524
Roman <47788675+Roman-Yarinski@users.noreply.github.com>
2020-07-09 07:33:18 +0300

    feat: add page404

--- src/components/error404/Error404.js
+++ src/components/error404/Error404.js
@@ -1,10 +1,12 @@
 import Component from '../Component';
 
+import './error404.scss'
+
 class Error404 extends Component {}
 
 const error404 = new Error404({
   selector: 'main',
-  template: '<h1>404 Page</h1><a class="btn btn-primary" href="#">На главную</a>',
+  template: '<h1 class="text-center">Oops, something went wrong.</h1><h4 class="text-center">Here you won’t learn new words.</h4><h3 class="text-center">Better return to the main page and you will continue to study English with RsLang.</h3><a class="btn btn-primary" href="#">HOME</a>',
 });
 
 export default error404;
