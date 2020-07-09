c3f73b12a33d4c169a9a887a96e0c8c375dc54e2
Roman <47788675+Roman-Yarinski@users.noreply.github.com>
2020-07-09 17:12:20 +0300

    fix: problems with styles

--- src/components/error404/Error404.js
+++ src/components/error404/Error404.js
@@ -6,7 +6,7 @@ class Error404 extends Component {}
 
 const error404 = new Error404({
   selector: 'main',
-  template: '<h1 class="text-center">Oops, something went wrong.</h1><h4 class="text-center">Here you won’t learn new words.</h4><h3 class="text-center">Better return to the main page and you will continue to study English with RsLang.</h3><a class="btn btn-primary" href="#">HOME</a>',
+  template: '<div class="wrap"><h1 class="error-page text-center">Oops, something went wrong.</h1><h4 class="text-center">Here you won’t learn new words.</h4><h3 class="text-center">Better return to the main page and you will continue to study English with RsLang.</h3><a class="error-page-btn btn btn-primary" href="#">HOME</a></div>',
 });
 
 export default error404;
