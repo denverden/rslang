f10ce9861d33b7ef35b43a6efeb502801aaaa8a5
Roman <47788675+Roman-Yarinski@users.noreply.github.com>
2020-07-10 05:56:53 +0300

    refactor: refactor code

--- src/components/error404/Error404.js
+++ src/components/error404/Error404.js
@@ -1,12 +1,19 @@
 import Component from '../Component';
 
-import './error404.scss'
+import './error404.scss';
+import img404 from './404_bg.png';
 
 class Error404 extends Component {}
 
 const error404 = new Error404({
   selector: 'main',
-  template: '<div class="wrap"><h1 class="error-page text-center">Oops, something went wrong.</h1><h4 class="text-center">Here you won’t learn new words.</h4><h3 class="text-center">Better return to the main page and you will continue to study English with RsLang.</h3><a class="error-page-btn btn btn-primary" href="#">HOME</a></div>',
+  template: `
+  <div class="wrap" style="background-image: url('${img404}')">
+    <h1 class="error-page text-center">Oops, something went wrong.</h1>
+    <h4 class="text-center">Here you won’t learn new words.</h4>
+    <h3 class="text-center">Better return to the main page and you will continue to study English with RsLang.</h3>
+    <a class="error-page-btn btn btn-primary" href="#">HOME</a>
+  </div>`,
 });
 
 export default error404;
