9c435f533e35e9b4dc788a8fd6571f2da89d25e6
Denis Novikov <denverden@mail.ru>
2020-07-10 23:16:41 +0300

    fix: 404 page styles

--- src/components/error404/Error404.js
+++ src/components/error404/Error404.js
@@ -8,11 +8,12 @@ class Error404 extends Component {}
 const error404 = new Error404({
   selector: 'main',
   template: `
-  <div class="wrap" style="background-image: url('${img404}')">
+  <div class="wrap">
     <h1 class="error-page text-center">Oops, something went wrong.</h1>
-    <h4 class="text-center">Here you won’t learn new words.</h4>
-    <h3 class="text-center">Better return to the main page and you will continue to study English with RsLang.</h3>
-    <a class="error-page-btn btn btn-primary" href="#">HOME</a>
+    <h6 class="text-center">Here you won’t learn new words.</h6>
+    <h6 class="text-center">Better return to the main page and you will continue to study English with RsLang.</h6>
+    <img class="img-fluid" src="${img404}" alt="error404">
+    <a class="error-page-btn btn btn-primary" href="#learn">TO LEARN</a>
   </div>`,
 });
 
