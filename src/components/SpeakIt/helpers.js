9131367ad342d67551b7d71e1c154c2fae826032
lena zamnius <lena.zamnius@gmail.com>
2020-07-14 20:25:49 +0300

    feat: implement update user dictionary with learned words

--- src/components/SpeakIt/helpers.js
+++ src/components/SpeakIt/helpers.js
@@ -1,3 +1,5 @@
+import AppStore from '../AppStore';
+
 export function clockToString(date) {
   const year = date.getFullYear();
   const monthNum = (date.getMonth() < 10) ? `0${(date.getMonth() + 1)}` : (date.getMonth() + 1);
@@ -31,7 +33,138 @@ export function playAudio(audioCSSClass, audioURL) {
   audio.setAttribute('src', audioURL);
 
   audio.onerror = () => {
-    // eslint-disable-next-line no-alert
-    alert('Error loading audio');
+    AppStore.viewMessage('alert-danger', 'Failed to load audio');
   };
+}
+
+export async function getUserWordIdsArr() {
+  const userId = localStorage.getItem('userId');
+  const token = localStorage.getItem('userToken');
+  const url = `${AppStore.apiUrl}/users/${userId}/words`;
+  const userWordIdsArr = [];
+
+  try {
+    const rawResponse = await fetch(url, {
+      method: 'GET',
+      withCredentials: true,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: 'application/json',
+      },
+    });
+    const content = await rawResponse.json();
+
+    if (content.error) AppStore.viewMessage('alert-danger', 'Failed to load all user words');
+
+    content.forEach((val) => userWordIdsArr.push(val.wordId));
+  } catch (error) {
+    AppStore.viewMessage('alert-danger', 'Failed to load all user words');
+  }
+
+  return userWordIdsArr;
+}
+
+async function getUserWordInfo(wordId) {
+  const userId = localStorage.getItem('userId');
+  const token = localStorage.getItem('userToken');
+  let wordInfoObj;
+
+  try {
+    const rawResponse = await fetch(`${AppStore.apiUrl}/users/${userId}/words/${wordId}`, {
+      method: 'GET',
+      withCredentials: true,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: 'application/json',
+      },
+    });
+    wordInfoObj = await rawResponse.json();
+
+    if (wordInfoObj.error) AppStore.viewMessage('alert-danger', 'Word info search failed');
+  } catch (error) {
+    AppStore.viewMessage('alert-danger', 'Word info search failed');
+  }
+
+  return wordInfoObj;
+}
+
+async function postNewUserWord(wordId, wordObj) {
+  const userId = localStorage.getItem('userId');
+  const token = localStorage.getItem('userToken');
+
+  try {
+    const res = await fetch(`${AppStore.apiUrl}/users/${userId}/words/${wordId}`, {
+      method: 'POST',
+      withCredentials: true,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(wordObj),
+    });
+    const response = await res.json();
+
+    if (response.error) AppStore.viewMessage('alert-danger', 'Failed to create new user word');
+  } catch (err) {
+    AppStore.viewMessage('alert-danger', 'Failed to create new user word');
+  }
+}
+
+export async function updateUserWord(wordId, guess) {
+  const userId = localStorage.getItem('userId');
+  const token = localStorage.getItem('userToken');
+  const wordObj = await getUserWordInfo(wordId);
+
+  delete wordObj.id;
+  delete wordObj.wordId;
+  wordObj.optional.time = clockToString(new Date());
+
+  if (guess) {
+    wordObj.optional.ratio += 1;
+    wordObj.optional.success += 1;
+  } else {
+    wordObj.optional.ratio -= 1;
+    wordObj.optional.error += 1;
+  }
+
+  try {
+    const res = await fetch(`${AppStore.apiUrl}/users/${userId}/words/${wordId}`, {
+      method: 'PUT',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(wordObj),
+    });
+
+    const result = await res.json();
+
+    if (result.error) AppStore.viewMessage('alert-danger', 'Failed to update word statistics');
+  } catch (err) {
+    AppStore.viewMessage('alert-danger', 'Failed to update word statistics');
+  }
+}
+
+export async function createUserWord(wordId, guess) {
+  const wordObj = {
+    difficulty: 'string',
+    optional: {
+      deleted: false,
+      time: clockToString(new Date()),
+    },
+  };
+
+  if (guess) {
+    wordObj.optional.ratio = 1;
+    wordObj.optional.success = 1;
+    wordObj.optional.error = 0;
+  } else {
+    wordObj.optional.ratio = -1;
+    wordObj.optional.success = 0;
+    wordObj.optional.error = 1;
+  }
+
+  await postNewUserWord(wordId, wordObj);
 }
