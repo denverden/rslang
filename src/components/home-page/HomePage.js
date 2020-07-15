84e7967e36a95126eb557db7594218eba1d1e15d
Roman <47788675+Roman-Yarinski@users.noreply.github.com>
2020-07-15 22:33:20 +0300

    feat: add new section "About" and fix problems with l-out

--- src/components/home-page/HomePage.js
+++ src/components/home-page/HomePage.js
@@ -2,6 +2,7 @@ import Component from '../Component';
 import './PromoStyle.scss';
 import gamesImg from './images/games.png';
 import settingsImg from './images/settings.png';
+import TeamIcon from './images/team.svg';
 
 class HomePage extends Component {
   afterRender() {
@@ -59,7 +60,7 @@ class HomePage extends Component {
 const homePage = new HomePage({
   selector: 'main',
   template: `<h1 class="welcome">Welcome to RS Lang!</h1>
-  <h6 class="welcome">Here you can look through general features of our website.</h6>
+  <h4 class="welcome">Here you can look through general features of our website.</h4>
   <section class="slider">
   <button class="slider__prev-button">
   </button>
@@ -87,26 +88,26 @@ const homePage = new HomePage({
   <button class="slider__next-button">
   </button>
 </section>
-<h1 class="info-text">Our opportunities and advantages:</h1>
+<h2 class="info-text">Our opportunities and advantages:</h2>
 <section class="info">
   <div class="info__item">
-    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Eo_circle_light-blue_white_checkmark.svg/1024px-Eo_circle_light-blue_white_checkmark.svg.png" alt="pic1">
+    <img src="https://img.icons8.com/ios/250/000000/ok.png" alt="pic1">
     <h3>A personal account that helps to keep progress</h3>
   </div>
   <div class="info__item">
-  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Eo_circle_light-blue_white_checkmark.svg/1024px-Eo_circle_light-blue_white_checkmark.svg.png" alt="pic2">
+  <img src="https://img.icons8.com/ios/250/000000/ok.png" alt="pic2">
     <h3>Interval repetition technique for fixing in memory of the studied words</h3>
   </div>
   <div class="info__item">
-  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Eo_circle_light-blue_white_checkmark.svg/1024px-Eo_circle_light-blue_white_checkmark.svg.png" alt="pic3">
+  <img src="https://img.icons8.com/ios/250/000000/ok.png" alt="pic3">
     <h3>Cognitive mini-game, allowing to expand vocabulary</h3>
   </div>
   <div class="info__item">
-    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Eo_circle_light-blue_white_checkmark.svg/1024px-Eo_circle_light-blue_white_checkmark.svg.png" alt="pic4">
+    <img src="https://img.icons8.com/ios/250/000000/ok.png" alt="pic4">
     <h3>Statistics to track your progress</h3>
   </div>
   <div class="info__item">
-    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Eo_circle_light-blue_white_checkmark.svg/1024px-Eo_circle_light-blue_white_checkmark.svg.png" alt="pic5">
+    <img src="https://img.icons8.com/ios/250/000000/ok.png" alt="pic5">
     <h3>A variety of settings for the convenience of using the site</h3>
   </div>
 </section>
@@ -128,7 +129,15 @@ const homePage = new HomePage({
     <h3>Example Rule 4</h3>
   </div>
 </section>
-<h1 class="start">So, what are you waiting for? Just <a href="http://localhost:3000/#sign-in">start</a> it right now!</h1>`,
+<section class="about">
+  <h2>About Team</h2>
+  <img " src="${TeamIcon}" alt="TeamIcon">
+  <h3>The RsLang app was created by a team of RSSchool students as a learning project. 
+    More information can be found on the 
+    <a href="https://rslang-team62-denverden.netlify.app/#about">About team</a> page.
+  </h3>
+</section>
+<h2 class="start">So, what are you waiting for? Just <a href="https://rslang-team62-denverden.netlify.app/#sign-in">start</a> it right now!</h2>`,
 });
 
 export default homePage;
